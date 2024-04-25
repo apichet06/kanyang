a73b2d11e6820c62366ea30093fc87a6af3a9673
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-04-25 19:01:37 +0700

    6rd commit

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,2 +1,2 @@
-// export const api = "https://kanyangapi-production.up.railway.app/api";
-export const api = "http://localhost:3310/api"
+export const api = "https://kanyangapi-production.up.railway.app/api";
+// export const api = "http://localhost:3310/api"
