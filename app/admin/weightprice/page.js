c7d7030e24aa5bd4bb9a400b1e99ab8a52aad321
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-04-25 10:32:24 +0700

    6rd commit

--- app/admin/weightprice/page.js
+++ app/admin/weightprice/page.js
@@ -10,13 +10,13 @@ export default function RubberPrice() {
                 <div className="row">
                     <div className='col-md-12 mt-5'>
                         <div className='row justify-content-center'>
-                            <div className='col-md-9 text-end mb-3'>
+                            <div className='col-md-10 text-end mb-3'>
                                 <button type="button" className="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal">
                                     เพิ่ม
                                 </button>
                                 <hr />
                             </div>
-                            <div className="col-md-9">
+                            <div className="col-md-10">
                                 <div className="card">
                                     <div className="card-body">
                                         <Datatable />
