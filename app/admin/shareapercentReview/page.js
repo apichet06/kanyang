b206f1b0c7a69021b210b84031c8d3f09ca021e7
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-05-06 22:22:12 +0700

    7rd commit

--- app/admin/shareapercentReview/page.js
+++ app/admin/shareapercentReview/page.js
@@ -9,9 +9,10 @@ import { formatPrice } from '../../utils/allfunctions';
 
 const columns = [
     { name: 'ปี', selector: row => row.r_rubber_year, width: '65px' },
-    { name: 'เลขหุ้น', selector: row => row.u_number, width: '110px' },
+    { name: 'ID', selector: row => row.u_number, width: '110px' },
+    { name: 'เลขหุ้น', selector: row => row.u_share_id, width: '110px' },
     { name: 'ชื่อ-สกุล', selector: row => row.u_title + '' + row.u_firstname + ' ' + row.u_lastname, width: '175px' },
-    { name: 'ที่อยู่', selector: row => row.u_address, width: '200px' },
+    { name: 'ที่อยู่', selector: row => row.u_address, width: '410px' },
     { name: 'จำนวนหุ้น', selector: row => formatPrice(row.u_share) },
     { name: 'เปอร์เซ็นหุ้น', selector: row => (row.percent) + '%' },
     { name: 'เงินปันผลหุ้น', selector: row => formatPrice(row.Sumpercentshare) },
@@ -25,42 +26,82 @@ const columns = [
 export default function Datatable() {
     const [data, setData] = useState([]);
     const [pending, setPending] = useState(true);
+    const [users, setUsers] = useState([])
+    const [percentYears, setPercentYears] = useState([])
+
+    const userData = useCallback(async () => {
+        try {
+            const response = await axios.get(api + "/users")
+            if (response.status === 200)
+                setUsers(response.data.data)
+        } catch (error) {
+            throw error
+        }
+
+
+    }, [api])
+
+    const percentYear = useCallback(async () => {
+        try {
+            const response = await axios.get(api + "/sharepercent")
+            if (response.status === 200)
+                setPercentYears(response.data.data)
+        } catch (error) {
+            throw error
+        }
+    }, [api])
 
     const showData = useCallback(async () => {
 
-        const response = await axios.get(api + "/sharepercent/share");
-
-        if (response.status === 200) {
-
-            setData(response.data.data);
-            setPending(false);
-        } else {
-            throw new Error("ไม่พบข้อมูล");
+        try {
+            const response = await axios.get(api + "/sharepercent/share");
+            if (response.status === 200) {
+                setData(response.data.data);
+                setPending(false);
+            }
+        } catch (error) {
+            throw error
         }
 
     }, [api])
 
     useEffect(() => {
         showData();
-    }, [showData])
+        userData()
+        percentYear()
+    }, [showData, userData, percentYear])
 
 
     return (
         <>
-            <div className="container">
-                <div className="row ">
-                    <div className='col-md-12 mt-5'>
-                        <div className='row justify-content-center'>
-                            <h4>รายงานเงินปันผลประจำปี</h4>
-                            <hr />
-                            <DataTable
+            <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div class="col-auto mt-5">
+                        <input className="form-control" list="percentYear" placeholder="เลือกปีปันผล..." />
+                        <datalist id="percentYear">
+                            {percentYears.map(p => (
+                                <option value={p.s_year}></option>
+                            ))}
+                        </datalist>
+                    </div>
+                    <div class="col-auto mt-5">
+                        <input class="form-control" list="user" id="u_number" placeholder="รหัสพนักงาน" />
+                        <datalist id="user">
+                            {users.map(user => (
+                                <option value={user.u_firstname}></option>
+                            ))}
 
-                                columns={columns}
-                                data={data}
-                                pagination
-                                progressPending={pending}
-                            />
-                        </div>
+                        </datalist>
+                    </div>
+                    <div className='col-md-10 mt-5'>
+                        <h4>รายงานเงินปันผลประจำปี</h4>
+                        <hr />
+                        <DataTable
+                            columns={columns}
+                            data={data}
+                            pagination
+                            progressPending={pending}
+                        />
                     </div>
                 </div>
             </div>
