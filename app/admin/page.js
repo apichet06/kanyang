de52999570e431b87b907e6d51264605867a19b9
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-05-18 16:21:55 +0700

    9rd commit

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,16 +1,68 @@
 'use client'
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import Chart from 'chart.js/auto';
+import { api } from '../utils/config';
+import axios from 'axios';
+
+
 export default function ChartPage() {
+
+    const [labal, setLabel] = useState([])
+
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await axios.get(api + '/rubberprice/data/chart');
+            const data = response.data.data;
+            console.log(data);
+            const sortedData = data.sort((a, b) => parseInt(a.m_number) - parseInt(b.m_number));
+
+            const labels = sortedData.map(month => month.m_name);
+            setLabel(labels)
+
+            const prices = sortedData.map(month =>
+                month.data.map(entry => parseFloat(entry.r_rubber_price))
+            );
+
+            const datasets = prices.map((priceArr, index) => ({
+                label: labels[index],
+                data: priceArr,
+                borderWidth: 1
+            }));
+
+
+
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }, [api]);
+
+
+    useEffect(() => {
+        fetchData()
+
+    }, [fetchData]);
+
+
+
 
 
     const chartData = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        // labels: ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'],
+        labels: labal,
         datasets: [{
-            label: 'ยอดขายยางพารา',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'r_rubber_date',
+            data: ['', '', '', '', '33', '', '', '', '', '', '', ''],
+            borderWidth: 1
+        }, {
+            label: 'r_rubber_date',
+            data: ['', '', '', '', '36', '', '', '', '', '', '', '10'],
+            borderWidth: 1
+        }, {
+            label: 'r_rubber_date',
+            data: ['', '', '', '', '41', '', '', '7', '', '', '', '11'],
             borderWidth: 1
         }]
     };
@@ -27,7 +79,8 @@ export default function ChartPage() {
                 anchor: 'end',
                 align: 'top',
                 formatter: (value, context) => {
-                    return context.chart.data.labels[context.dataIndex] + ': ' + value;
+                    return value;
+                    // return context.chart.data.labels[context.dataIndex] + ': ' + value;
                 }
             }
         }
@@ -40,7 +93,7 @@ export default function ChartPage() {
                 <div className="col-md-12 mt-5">
                     <div className="card">
                         <div className="card-body">
-                            <h2>ยอดขายยางพารา</h2>
+                            <h2>ราคาประมูลยางพารา</h2>
                             <Bar data={chartData} options={chartOptions} plugins={[ChartDataLabels]} height="70%" />
                         </div>
                     </div>
