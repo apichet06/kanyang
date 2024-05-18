881868b151e858d76728ad4df9272688d0c4a92f
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-05-18 09:38:01 +0700

    9rd commit

--- app/admin/rubberPriceReport/ComponentToPrint.js
+++ app/admin/rubberPriceReport/ComponentToPrint.js
@@ -6,12 +6,12 @@ import { formatDate, formatPrice } from '../../utils/allfunctions';
 const ComponentToPrint = forwardRef(({ data }, ref) => (
     <div ref={ref} >
         {data.map((row, index) => (<>
+            <div className='fs-5 fw-bolder'> ชื่อ : {row.username}</div>
+            <div >ที่อยู่ : {row.Address}</div>
+            <div >วันที่ขาย : {formatDate(row.r_rubber_date)} รอบขายประจำเดือน : รอบที่ {row.r_around}</div>
+            <div className='fs-6'>ราคาประมูล : {formatPrice(row.r_rubber_price)} น้ำหนักรวม : {row.w_weigth} กิโลกรัม </div>
+            <h5 className='fw-bold'>รวมเป็นเงิน : {formatPrice(row.w_price)} </h5>
             <hr key={index} />
-            <div className='fs-6 fw-bolder' style={{ paddingLeft: '0.5cm' }}> ชื่อ : {row.username}</div>
-            <div style={{ paddingLeft: '0.5cm' }}>ที่อยู่ : {row.Address}</div>
-            <div style={{ paddingLeft: '0.5cm' }}>วันที่ขาย : {formatDate(row.r_rubber_date)} รอบขายประจำเดือน : รอบที่ {row.r_around}</div>
-            <div style={{ paddingLeft: '0.5cm' }} className='fs-6'>ราคาประมูล : {formatPrice(row.r_rubber_price)} น้ำหนักรวม : {row.w_weigth} กิโลกรัม </div>
-            <h5 style={{ paddingLeft: '0.5cm' }} className='fw-bold'>รวมเป็นเงิน : {formatPrice(row.w_price)} </h5>
         </>))}
     </div>
 
--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={prompt.className}>
         {children}
         <InstallBootstrap />
