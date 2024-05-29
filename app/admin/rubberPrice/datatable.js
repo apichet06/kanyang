82d88883796c2fee0dcd2e8d1692fba096a0915c
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-05-29 10:10:07 +0700

    9rd commit

--- app/admin/rubberPrice/datatable.js
+++ app/admin/rubberPrice/datatable.js
@@ -18,7 +18,7 @@ export default function datatable() {
         { name: 'ID', selector: row => row.autoID, width: '80px' },
         { name: 'รอบขายยางพารา', selector: row => row.r_around, width: '120px' },
         { name: 'วันขาย', selector: row => formatDate(row.r_rubber_date), width: '130px' },
-        { name: 'ราคาขายยางพารา', selector: row => formatPrice(row.r_rubber_price), width: '130px' },
+        { name: 'ราคาประมูลได้', selector: row => formatPrice(row.r_rubber_price), width: '130px' },
         { name: 'ผู้บันทึก', selector: row => row.username, width: '190px' },
         {
             name: "จัดการ",
