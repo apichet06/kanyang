a6292f102038ec50596eee8747ac4f5e38df03fb
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2025-02-21 01:41:02 +0700

    updateApp

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,2 +1,2 @@
-export const api = "https://kanyangapi-production.up.railway.app/api";
+export const api = "https://www.หนองป่าอ้อยยางพารา.com/api";
 // export const api = "http://localhost:3310/api"
