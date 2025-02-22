d0ceecc4c07c60a6bd84da361dcd1d50356eca0b
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2025-02-22 12:09:22 +0700

    updateHistory

--- app/admin/rubberPricehistory/page.js
+++ app/admin/rubberPricehistory/page.js
@@ -5,8 +5,7 @@ import axios from 'axios';
 import { api } from "../../utils/config";
 import { formatPrice, formatDate, formatDateTime } from '../../utils/allfunctions';
 
-export default function page() {
-
+export default function Page() {
 
     const columns = [
         { name: 'ID', selector: row => row.w_number },
@@ -20,63 +19,63 @@ export default function page() {
         { name: 'วันที่บันทึก', selector: row => formatDateTime(row.w_datetime), width: '175px' },
     ];
 
-
     const [data, setData] = useState([]);
-    const [pending, setPending] = useState(true);
+    const [pending, setPending] = useState(false); // เปลี่ยนค่าเริ่มต้นเป็น false
     const [rubberprice, setRubberprice] = useState([]);
-    const [u_firstname, setUfirstname] = useState('')
+    const [u_firstname, setUfirstname] = useState('');
     const [r_number, setRnumber] = useState('');
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
 
-    const handlerubberpriceChange = useCallback(async () => {
+    const fetchRubberPrice = useCallback(async () => {
         try {
-            const response = await axios.get(api + '/rubberprice')
+            const response = await axios.get(api + '/rubberprice');
             if (response.status === 200) {
-
-                setRubberprice(response.data.data)
+                setRubberprice(response.data.data);
             }
-
         } catch (error) {
             console.log(error);
         }
-    }, [api])
+    }, [api]);
 
-    const showData = useCallback(async () => {
+    const showData = async () => {
+        if (!r_number || !u_firstname) return;
 
-        const Data = {
-            r_number, u_firstname
-        }
-        const response = await axios.post(api + "/weightprice/weight", Data);
-
-        if (response.status === 200) {
-            setData(response.data.data);
-            setPending(false);
-        } else {
-            throw new Error("ไม่พบข้อมูล");
-        }
-
-    }, [api, r_number, u_firstname])
-
-
-    const userData = useCallback(async () => {
+        setPending(true);
         try {
-            const response = await axios.get(api + "/users")
-            if (response.status === 200)
-                setUsers(response.data.data)
+            const Data = { r_number, u_firstname };
+            const response = await axios.post(api + "/weightprice/weight", Data);
+            if (response.status === 200) {
+                setData(response.data.data);
+            }
         } catch (error) {
-            throw error
+            console.error(error);
+        } finally {
+            setPending(false);
         }
+    };
+
+    const fetchUsers = useCallback(async () => {
+        try {
+            const response = await axios.get(api + "/users");
+            if (response.status === 200) {
+                setUsers(response.data.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchUsers();
+        fetchRubberPrice();
+    }, [fetchUsers, fetchRubberPrice]);
 
 
-    }, [api])
-
-    const dateSearch = rubberprice.filter((item) => {
-        return item.r_number == r_number;
-    });
+    const dateSearch = rubberprice.filter(item => item.r_number === r_number);
 
     const downloadExcelFile = async () => {
         try {
-            const Data = { r_number, u_firstname }
+            const Data = { r_number, u_firstname };
             const response = await axios.post(api + "/weightprice/Export", Data, { responseType: 'blob' });
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
@@ -88,50 +87,45 @@ export default function page() {
         } catch (error) {
             console.error('Error downloading Excel file:', error);
         }
-    }
-
-
-
-    useEffect(() => {
-        showData();
-        userData()
-        handlerubberpriceChange()
-    }, [showData, userData, handlerubberpriceChange])
+    };
 
     return (
         <>
             <div className="container mb-5">
                 <div className="row justify-content-center">
-                    <div className="col-md-4 mt-5">
+                    <div className="col-md-3 mt-md-5 mt-lg-5 mt-sm-5">
                         <select className="form-select" onChange={(e) => setRnumber(e.target.value)} required>
                             <option value="">เลือกรอบขาย/ราคาประมูลยาง</option>
                             {rubberprice.map(item => (
-                                <option key={item.r_number} value={item.r_number}>{formatDate(item.r_rubber_date) + ' รอบ ' + item.r_around + ' ราคาประมูล ' + item.r_rubber_price}</option>
+                                <option key={item.r_number} value={item.r_number}>
+                                    {formatDate(item.r_rubber_date) + ' รอบ ' + item.r_around + ' ราคาประมูล ' + item.r_rubber_price}
+                                </option>
                             ))}
                         </select>
                     </div>
-                    <div className="col-md-4 mt-5">
+                    <div className="col-md-3 mt-md-5 mt-lg-5 mt-sm-2">
                         <input className="form-control" list="user" placeholder="ค้นหาชื่อสมาชิก" onChange={e => setUfirstname(e.target.value)} />
                         <datalist id="user">
                             {users.map(user => (
                                 <option key={user.u_number} value={user.u_firstname}></option>
                             ))}
-
                         </datalist>
+                    </div>
+                    <div className="col-md-1 mt-md-5 mt-lg-5 mt-sm-2 col-sm-12 text-center">
+                        <button className="btn btn-primary" onClick={showData}>ค้นหา</button>
                     </div>
                 </div>
                 <div className="row justify-content-center">
-                    <div className='col-md-12 mt-5'>
+                    <div className="col-md-12 mt-5">
                         <hr />
-                        <div className='row'>
-                            <div className='col-md-7'>
-                                <h4>ประวัติการขายยางพาราทั้งหมด  {dateSearch.length > 0 ? 'ประจำรอบ ' + formatDate(dateSearch[0].r_rubber_date) : ''}</h4>
+                        <div className="row">
+                            <div className="col-md-7">
+                                <h4>ประวัติการขายยางพาราทั้งหมด {dateSearch.length > 0 ? 'ประจำรอบ ' + formatDate(dateSearch[0].r_rubber_date) : ''}</h4>
                             </div>
-                            <div className='col-md-5 text-end'>
-                                <button className='btn btn-sm btn-secondary' onClick={downloadExcelFile}>ออกรายงาน Excel</button>
+                            <div className="col-md-5 text-end">
+                                <button className="btn btn-sm btn-secondary" onClick={downloadExcelFile}>ออกรายงาน Excel</button>
                             </div>
                         </div>
-                        <hr />
                         <hr />
                         <DataTable
                             columns={columns}
@@ -142,7 +136,6 @@ export default function page() {
                     </div>
                 </div>
             </div>
-
         </>
-    )
+    );
 }
--- app/utils/config.js
+++ app/utils/config.js
@@ -1,2 +1,2 @@
-export const api = "https://www.หนองป่าอ้อยยางพารา.com/api";
+export const api = "https://www.xn--72ca6cjqwak8dvaca7decb77aua.com/api";
 // export const api = "http://localhost:3310/api"
